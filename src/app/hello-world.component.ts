73c17f2fe7cb65398bec7539bf63b15038ab577d
ERIS RISO <erisriso@gmail.com>
2022-07-22 23:31:21 +0700

    Angular basic banget

---
+++ src/app/hello-world.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Logger } from './logger.service'
+
+@Component({
+    selector: 'hello-world',
+    templateUrl: './hello-world.component.html',
+    styles: ['h2,p{font-family: sans-serif;}']
+})
+export class HelloWorldComponent {
+    judul = "Hello Angular."
+    gambar = 'assets/images/erisriso.jpg'
+    aktif = false
+    count = 0
+
+    constructor(private logger: Logger){}
+
+    sayMessage(name){
+        alert('Hello '+ name.toUpperCase())
+    }
+
+    onEditClick(){
+        this.aktif = !this.aktif
+    }
+
+    Increment(){
+        this.logger.writeCount(this.count)
+        this.count++
+    }
+}
